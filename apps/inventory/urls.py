dc10f66adf299a6a18a77b56c622f19e73c48978
oilenosreg <oilenosreg@gmail.com>
2022-07-14 23:16:29 -0500

    Avance listado de equipos 14.07.22

--- apps/inventory/urls.py
+++ apps/inventory/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 # App modules.
-from .views import list_equipos, create_equipo
+from .views import list_equipos, create_equipo, edit_equipo
 
 
 app_name = 'inventory'
 urlpatterns = [
-    path('equipos/', list_equipos, name='list_equipo'),
+    path('equipos/', list_equipos, name='list_equipos'),
     path('equipos/nuevo/', create_equipo, name='create_equipo'),
+    path('equipos/modificar/<int:id>', edit_equipo, name='edit_equipo'),
 ]
--- apps/inventory/views.py
+++ apps/inventory/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_list_or_404, get_object_or_404
+from django.contrib import messages
 
 # App modules.
 from .models import Equipo
@@ -17,19 +18,58 @@ def list_equipos(request):
     return render(request, template, context)
 
 
+# TODO:   Mejorar la tabla de los equipos y sus componentes.
+#         ¿se debería usar un foreign key?
+# Procesador 	Advance 	802 	Computadora Portátil Epson 2201-61063-001 - Sn: #789**** - 7879787897 	SN:qqqqqqqqq 	7879787897 	Inoperativo 	Bueno 
 def create_equipo(request):
     if request.method == 'POST':
         form = CreateEquipmentForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('equipo:listar')
+
+            messages.success(
+                request, 
+                'Equipo registrado con éxito.')               
+            return redirect('inventory:list_equipos')
+        else:
+            messages.error(
+                request,
+                'No fue posible guardar los datos. Verifique la información \
+                ingresada.')
     else:
         form = CreateEquipmentForm()    
-    plantilla = 'inventory/create_equipo.html'
-    contexto = { 
+    template = 'inventory/create_equipo.html'
+    context = { 
         'form': form,
         'create': True,
         'pre_title': 'Parque Informático',
-        'title': 'Registrar nuevo equipo'
+        'title': 'Registrar nuevo equipo',
     }
-    return render(request, plantilla, contexto)
+    return render(request, template, context)
+
+
+def edit_equipo(request, id):
+    equipo = get_object_or_404(Equipo, id = id)
+    form = CreateEquipmentForm(request.POST or None, instance=equipo)
+    if form.is_valid():
+        form.save()
+        messages.success(
+            request, 
+            'Equipo modificado con éxito.')     
+
+        return redirect('inventory:list_equipos')
+    else:
+        messages.error(
+            request,
+            '''
+            No fue posible guardar los datos. Verifique la información \
+            ingresada.
+            ''')
+    template = 'inventory/create_equipo.html'
+    context = { 
+        'form': form,
+        'edit': True,
+        'pre_title': 'Parque Informático',
+        'title': 'Modificar equipo',
+    }    
+    return render(request, template, context)
